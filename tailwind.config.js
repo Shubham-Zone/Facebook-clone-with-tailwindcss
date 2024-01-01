c83f15705940d84acc7a29048928eff3e042f5ef
Shubham-Zone <154971588+Shubham-Zone@users.noreply.github.com>
2024-01-01 18:14:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+        13: '3.25rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
